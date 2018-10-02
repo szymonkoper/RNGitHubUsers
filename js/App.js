1a87d5fb6cddf596be78fbc0c50238da35cf2a2b
Szymon Koper <sz.koper@gmail.com>
2018-10-02 10:13:04 +0200

    Reorganized HomeScreen component structure

--- js/App.js
+++ js/App.js
@@ -1,5 +1,5 @@
 import { createStackNavigator } from 'react-navigation';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/Home/HomeScreen';
 
 export default createStackNavigator({
   Home: {
--- js/screens/Home/HomeScreen.js
+++ js/screens/Home/HomeScreen.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
 import {
-  Platform, StyleSheet, Text, View,
+  Platform, Text, View,
 } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import styles from './styles';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
