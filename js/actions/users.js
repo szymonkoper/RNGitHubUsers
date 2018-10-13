85ce6c2895cce85e841bb8ca78889550e3abac67
Szymon Koper <sz.koper@gmail.com>
2018-10-13 15:27:44 +0200

    Increase fetched users count to 100

--- js/actions/users.js
+++ js/actions/users.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
 query ($name: String!) {
-  search(query: $name, type: USER, last: 10) {
+  search(query: $name, type: USER, last: 100) {
     nodes {
       ... on RepositoryOwner {
         login
