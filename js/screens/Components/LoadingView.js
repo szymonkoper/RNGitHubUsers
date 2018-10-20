4056f3263404d0fc8a79ab0596e3cb0e7d04eee7
Szymon Koper <sz.koper@gmail.com>
2018-10-20 13:33:00 +0200

    Remove one level of view nesting in Loading animation

--- js/screens/Components/LoadingView.js
+++ js/screens/Components/LoadingView.js
@@ -5,12 +5,10 @@ import LottieView from 'lottie-react-native';
 const animationSource = require('../../../resources/lottie/loading.json');
 
 export default () => (
-  <View>
-    <LottieView
-      style={{ width: 200, height: 60, alignSelf: 'center' }}
-      source={animationSource}
-      autoPlay
-      loop
-    />
-  </View>
+  <LottieView
+    style={{ width: 200, height: 60, alignSelf: 'center' }}
+    source={animationSource}
+    autoPlay
+    loop
+  />
 );
